446a827d2064f300d2554e40ccbcc49461cded4b
casey <cmatthew@uccs.edu>
2017-11-06 01:46:51 -0700

    player report method

--- tradeSim.py
+++ tradeSim.py
@@ -12,20 +12,27 @@ testing = False
 
 def animate(i):
 	p = random.randint(0, 10)
+	l = random.randint(0,10)
 	dataY[p] += 1
+	dataY[l] -= 1
 	ax1.clear()
 	ax1.bar(dataX, dataY)
+
+def buildPlayerReport(playerData):
+	dataX = range(0,max(playerData)+1)
+	dataY = [0] * (max(playerData)+1)
+	for pWorth in playerData:
+		dataY[pWorth] += 1
+	return dataX, dataY
 
 player_worth = [10] * numberPlayers
 
 fig = plt.figure()
-#dataX = [1,2,3,4,5,6,7,8,9,10]
-dataX = range(0,max(player_worth)+1)
-print dataX
-dataY = [0] * (max(player_worth) + 1)
 ax1 = fig.add_subplot(1,1,1)
 
+dataX = range(0,max(player_worth)+1)
+dataY = [0] * (max(player_worth) + 1)
+playerData = [10] * numberPlayers
 
-
-ani = animation.FuncAnimation(fig, animate, 25, interval=1000)
+ani = animation.FuncAnimation(fig, animate, 25, interval=500)
 plt.show()
