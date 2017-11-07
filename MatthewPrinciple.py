91dc57711f4a3525db8ecb76d39deb1945ed38ab
casey <cmatthew@uccs.edu>
2017-11-06 21:10:43 -0700

    script cleaned up and finalized

--- MatthewPrinciple.py
+++ MatthewPrinciple.py
@@ -1,30 +1,33 @@
-import numpy as np
+# Casey Matthews
+# 11/06/17
+# Random trading game simulation that demonstrates the economic
+# phenomenon known as the Matthew Principle. A pool of players
+# are given a fixed amount of capital and are then randomly
+# paired for trading. In each transaction, the player has an
+# equal chance to either gain or lose a dollar. You will see
+# that in the early stages, the wealth becomes distributed
+# normally but as the game progresses it collapses into a
+# Pareto distribution. To those who have everything,more
+# will be given and from those who have nothing everything
+# will be taken -- this is the Matthew Principle.
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
 
 ## Runtime Variables ###
-numberPlayers = 1000
-tradingRounds = 30
-startingCash  = 50
-proportion = 2
-frame_rate = 10 #milliseconds
+numberPlayers = 1000 	# number of players
+startingCash  = 10 		# $ players start with
+proportion = 4  		# 1 / n player trade each round.
+frame_rate = 50 		#milliseconds
 ########################
-
-def animate(i):
-	p = random.randint(0, 10)
-	l = random.randint(0,10)
-	dataY[p] += 1
-	dataY[l] -= 1
-	ax1.clear()
-	ax1.bar(dataX, dataY)
 
 def animateTrading(i):
 	global broke, round_num
 	random.shuffle(player_worth)
 	for i in range(0,len(player_worth) / proportion, 2):
-		p = random.randint(0,100)
-		if p > 50:
+		p = random.randint(0,10000)
+		if p > 5000:
 			if player_worth[i+1] != 0:
 				player_worth[i] += 1
 				player_worth[i+1] -= 1
@@ -32,22 +35,13 @@ def animateTrading(i):
 			if player_worth[i] != 0:
 				player_worth[i+1] += 1
 				player_worth[i] -= 1
-		# broke player exclusion
-		'''
-		if player_worth[i] == 0:
-			broke += 1
-			del player_worth[i]
-		if player_worth[i+1] == 0:
-			broke += 1
-			del player_worth[i+1]
-		'''
 
-		#build graph data
+	#build graph data
 	dataX, dataY = buildReport(player_worth)
 	ax1.clear()
 	ax1.bar(dataX,dataY)
-	ax1.set_xlabel('wealth in $')
-	ax1.set_ylabel("number of players")
+	ax1.set_xlabel('Wealth in $')
+	ax1.set_ylabel("Number of Players")
 	ax1.set_title("Random Trading: Round " + str(round_num))
 	round_num += 1
 
@@ -61,18 +55,13 @@ def buildReport(playerData):
 		dataY[pWorth] += 1
 	return dataX, dataY
 
-player_worth = [startingCash] * numberPlayers
-#global broke
-broke = 0
 round_num = 1
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
-
-
+player_worth = [startingCash] * numberPlayers
 dataX = range(0,max(player_worth)+1)
 dataY = [0] * (max(player_worth) + 1)
-playerData = [10] * numberPlayers
 
 ani = animation.FuncAnimation(fig, animateTrading, 25, interval=frame_rate)
 plt.show()
